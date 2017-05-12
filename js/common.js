09a2e765f9c981e985972c32e299f9837e6f7b07
Oleg <welikodub@gmail.com>
2017-05-12 23:37:31 +0200

    Update files

--- js/common.js
+++ js/common.js
@@ -1,15 +1,14 @@
 $(document).ready(function() {
 
+	$(".mc_item_wrap ul").each(function(){
+		$(this).after('<div class="mc_item_wrap_af"></div>');
+	});
 	$(".mc_toggle").click(function(){
-		var Obj = $(this).parent().parent();
-
-		Obj.children(".mc_item").append('<div class="mc_item_wrap_af"></div>');
-		$(".mc_item_wrap_af").fadeIn();
-		
-		Obj.children("ul").slideToggle(function(){
-			// Obj.children(".mc_item").append('<div class="mc_item_wrap_af"></div>');
-			// $(".mc_item_wrap_af").fadeIn();
-		});
+		$("body .mc_wrap .mc_item_wrap > ul, .mc_item_wrap_af").hide();
+		$(".mc_item_wrap").removeClass("active");
+		$(this).parent().parent().addClass("active");
+		$(this).parent().parent().children("ul").show();
+		$(this).parent().parent().children(".mc_item_wrap_af").show(0);
 	});
 
 
