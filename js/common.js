c962565cd10b008c5d31493555ef7e37d06d0af7
Oleg <welikodub@gmail.com>
2017-05-15 23:58:43 +0200

    Update lesson

--- js/common.js
+++ js/common.js
@@ -3,12 +3,21 @@ $(document).ready(function() {
 	$(".mc_item_wrap ul").each(function(){
 		$(this).after('<div class="mc_item_wrap_af"></div>');
 	});
+
+
 	$(".mc_toggle").click(function(){
-		$("body .mc_wrap .mc_item_wrap > ul, .mc_item_wrap_af").hide();
-		$(".mc_item_wrap").removeClass("active");
-		$(this).parent().parent().addClass("active");
-		$(this).parent().parent().children("ul").show();
-		$(this).parent().parent().children(".mc_item_wrap_af").show(0);
+
+		if ($(this).parent().parent().children("ul").is(":visible")) {
+			$(this).parent().parent().children("ul").slideUp();
+			$(this).parent().parent().children(".mc_item_wrap_af").hide();
+		} else {
+			$("body .mc_wrap .mc_item_wrap > ul, .mc_item_wrap_af").hide();
+			$(".mc_item_wrap").removeClass("active");
+			$(this).parent().parent().addClass("active");
+			$(this).parent().parent().children("ul").slideDown();
+			$(this).parent().parent().children(".mc_item_wrap_af").show();
+		}
+		
 	});
 
 
@@ -30,7 +39,7 @@ $(document).ready(function() {
 	//equalheight - одинаковая высота колонок
 	//Пример списка элементов:
 	//var eqElement = ".cat_container > div, .home_news > div"
-	var eqElement = ".element"
+	var eqElement = ".hi_item"
 	$(window).load(function(){equalheight(eqElement);}).resize(function(){equalheight(eqElement);});
 
 	//Masked Input Plugin
